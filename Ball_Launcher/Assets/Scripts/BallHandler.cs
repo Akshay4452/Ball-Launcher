83f36e04af3ed2f275e380715085941ca9e322fe
Akshay Manthekar <58980814+Akshay4452@users.noreply.github.com>
2022-11-24 12:30:48 +0530

    Ball Launching Done

--- Ball_Launcher/Assets/Scripts/BallHandler.cs
+++ Ball_Launcher/Assets/Scripts/BallHandler.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D currentBallRigidbody2d;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    private float detachDelay = 0.1f;
+    private bool isDragging;
     private Camera mainCamera;
     // Start is called before the first frame update
     void Start()
@@ -14,15 +18,44 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If we dont have rigidbody attached to the script then exit update loop
+        if(currentBallRigidbody2d == null) {
+            Debug.LogError("Ball Rigidbody is not attched in inspector");
+            return;}
+        if(currentBallSpringJoint == null) {
+           Debug.LogError("Ball Spring Joint is not attched in inspector");
+            return;} 
+
         if(!Touchscreen.current.primaryTouch.press.isPressed)
+        // When we released the touch
         {
+            if(isDragging)
+            {
+                LaunchBall();
+            }
+            isDragging = false;
             return;
         }
-        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
+        isDragging = true;
+        currentBallRigidbody2d.isKinematic = true;
+
+        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
         // we will use camera to convert screen space to world space
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
-        Debug.Log(worldPosition);
+        currentBallRigidbody2d.position = worldPosition; 
+    }
+    void LaunchBall()
+    {
+        currentBallRigidbody2d.isKinematic = false;
+        currentBallRigidbody2d = null; // After releasing the ball, it wont snap back to the touch location
+
+        Invoke(nameof(DetachBall), detachDelay);   
+    }
+    void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
